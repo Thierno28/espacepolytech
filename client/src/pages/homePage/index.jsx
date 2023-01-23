714a532d218cdfc409788d3181802f4cb0ab52a0
Thierno28 <thiernodiop@esp.sn>
2023-01-23 15:10:05 +0000

    pages

--- client/src/pages/homePage/index.jsx
+++ client/src/pages/homePage/index.jsx
@@ -1,11 +1,11 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
-import Navbar from "scenes/navbar";
-import UserWidget from "scenes/widgets/UserWidget";
-import MyPostWidget from "scenes/widgets/MyPostWidget";
-import PostsWidget from "scenes/widgets/PostsWidget";
+import Navbar from "pages/navbar";
+import UserWidget from "pages/widgets/UserWidget";
+import MyPostWidget from "pages/widgets/MyPostWidget";
+import PostsWidget from "pages/widgets/PostsWidget";
 // import AdvertWidget from "scenes/widgets/AdvertWidget";
-import FriendListWidget from "scenes/widgets/FriendListWidget";
+import FriendListWidget from "pages/widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
--- client/src/pages/profilePage/index.jsx
+++ client/src/pages/profilePage/index.jsx
@@ -2,11 +2,11 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import Navbar from "scenes/navbar";
-import FriendListWidget from "scenes/widgets/FriendListWidget";
-import MyPostWidget from "scenes/widgets/MyPostWidget";
-import PostsWidget from "scenes/widgets/PostsWidget";
-import UserWidget from "scenes/widgets/UserWidget";
+import Navbar from "pages/navbar";
+import FriendListWidget from "pages/widgets/FriendListWidget";
+import MyPostWidget from "pages/widgets/MyPostWidget";
+import PostsWidget from "pages/widgets/PostsWidget";
+import UserWidget from "pages/widgets/UserWidget";
 
 const ProfilePage = () => {
   const [user, setUser] = useState(null);
