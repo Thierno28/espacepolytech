cfa4ebdbc56abf16ccdb5ef26733031766d9f626
Thierno28 <thiernodiop@esp.sn>
2023-01-24 12:25:49 +0000

    first commit for websocket

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,6 @@
 import Post from "../models/Post.js";
 import User from "../models/User.js";
 
-
 /* CREATE */
 export const createPost = async (req, res) => {
   try {
@@ -30,7 +29,7 @@ export const createPost = async (req, res) => {
 /* READ */
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find().populate("User");
+    const post = await Post.find();
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
